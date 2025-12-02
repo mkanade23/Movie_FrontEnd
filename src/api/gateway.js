ef8d90f33bbee8390c0f71f976e2ef7d56dad6c1
Mithilkanade <mithilkanade@trayahealth.co>
2025-12-02 15:37:46 +0530

    Initial frontend commit

--- src/api/gateway.js
+++ src/api/gateway.js
@@ -1,6 +1,13 @@
+// import axios from "axios";
+
+// export const GATEWAY = axios.create({
+//   baseURL: "http://localhost:9090/api/movies", // API Gateway
+//   // you can set timeout, headers etc.
+// });
+
 import axios from "axios";
 
 export const GATEWAY = axios.create({
-  baseURL: "http://localhost:9090/api/movies", // API Gateway
-  // you can set timeout, headers etc.
+  baseURL: "https://movies-backend-49lv.onrender.com/api/movies",
 });
+
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,2 @@
-export const IMAGE_BASE = "http://localhost:9090/uploads/";
+// export const IMAGE_BASE = "http://localhost:9090/uploads/";
+export const IMAGE_BASE = "https://movies-backend-49lv.onrender.com/uploads/";
